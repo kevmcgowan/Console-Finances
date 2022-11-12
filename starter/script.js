2276719896d398e9e99d9807455d40bb62c000f1
Kevin.McGowan <Kevin.McGowan@constellis.com>
2022-11-12 17:49:16 +0300

    created initial code

---
+++ starter/script.js
@@ -0,0 +1,22 @@
+
+
+
+//Total Number of Months in the DataSet.   
+
+
+var profLoss = finances.map(d => d[1]);
+
+//Total Profit & Losses
+var sum = 0;
+var change = 0;
+
+
+console.log('Total Months: ' + finances.length)
+console.log('Total: $' )
+console.log('Average Change: ')
+console.log('Greatest Increase in Profits: $')
+console.log('Greatest Decrease in Profits: $')
+
+
+
+
